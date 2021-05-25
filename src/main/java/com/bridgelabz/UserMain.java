374bdd43fc0af43b916aff1b097b7723d31cf15f
datta1808 <bagaledatta18@gmail.com>
2021-05-25 20:47:02 +0530

    Added: Validator for First Name

--- src/main/java/com/bridgelabz/UserMain.java
+++ src/main/java/com/bridgelabz/UserMain.java
@@ -1,7 +1,10 @@
 package com.bridgelabz;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class UserMain {
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Registration System");
         UserRegistration user = new UserRegistration();
         user.SelectOption();
     }
